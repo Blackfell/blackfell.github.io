bc8b7c256b12d0596dda3fd2717834cd72a31fc5
blackfell <43939253+Blackfell@users.noreply.github.com>
2024-12-11 16:11:34 +0000

    Strap script for Ubuntu/Kali

---
+++ strap.sh
@@ -0,0 +1,310 @@
+#!/bin/bash
+
+add_line_if_not_exists() {
+    local line="$1"
+    local file="$2"
+
+    # Check if the line exists in the file
+    if ! grep -Fxq "$line" "$file"; then
+        # If the line does not exist, append it to the file
+        echo "$line" >> "$file"
+    fi
+}
+
+add_rc_path () {
+    # If you're using another shell you deserve what you get OK?
+    add_line_if_not_exists "export PATH=\$PATH:$1" "$HOME/.zshrc"
+    add_line_if_not_exists "export PATH=\$PATH:$1" "$HOME/.bashrc"
+}
+
+clone_or_update_repo() {
+    local REPO_URL="$1"
+    local REPO_NAME=$(basename -s .git "$REPO_URL")
+    if [ -n "$2" ]; then 
+	    local BRANCH="--branch $2"
+	    local REPO_NAME="${REPO_NAME}_$2"
+    fi
+    local TARGET_DIR="/opt/$REPO_NAME"
+    sudo mkdir -p $TARGET_DIR
+    sudo chown $USER:$USER $TARGET_DIR
+    if [ -z "$(ls -A "$TARGET_DIR")" ]; then
+        echo "$TARGET_DIR created, but empty, cloning..."
+        git clone $BRANCH "$REPO_URL" "$TARGET_DIR"
+    else
+        echo "Directory $TARGET_DIR already exists. Pulling latest changes..."
+        # Navigate to the directory and pull the latest changes
+        git -C "$TARGET_DIR" pull
+    fi
+
+}
+
+generic_setup() {
+    # Tools that get installed regardless
+    sudo apt install -y thefuck byobu vim flashrom nmap bashtop python3-pwntools esptool plocate golang-go docker.io rustup python3-venv pipx curl
+    rustup default stable
+    
+    #PMapper
+    pipx install principalmapper
+
+    # Kingst
+    
+    if [ ! -f /opt/kingstVIS/vis_linux ]; then 
+        sudo mkdir -p /opt/kingstVIS 
+        pushd /opt/kingstVIS
+        sudo chown -R $USER:$USER /opt/kingstVIS        
+        wget http://www.qdkingst.com/download/vis_linux
+        tar -xf vis_linux
+        if lsusb | grep -qi 77a1 ; then
+            echo "[!] ERROR: You may have a kingst device plugged in, not installing..."
+        else
+            pushd KingstVIS
+            sudo ./install.sh
+            popd
+        fi
+        popd
+    fi
+    
+    # Frida and objection
+    pipx install frida-tools
+    pipx install objection
+    if [ ! -f /opt/frida-server/frida-server-16.5.9-android-arm64.xz ]; then
+        sudo mkdir -p /opt/frida-server
+        sudo chown -R $USER:$USER /opt/frida-server
+        pushd /opt/frida-server
+        wget https://github.com/frida/frida/releases/download/16.5.9/frida-server-16.5.9-android-arm64.xz
+        wget https://github.com/frida/frida/releases/download/16.5.9/frida-server-16.5.9-android-arm.xz
+        wget https://github.com/frida/frida/releases/download/16.5.9/frida-server-16.5.9-android-x86.xz
+        wget https://github.com/frida/frida/releases/download/16.5.9/frida-server-16.5.9-android-x86_64.xz
+        popd
+    fi
+
+    # JLink
+    if [ ! /opt/JLink/JLink_Linux_x86_64.deb ]; then
+        sudo mkdir -p /opt/JLink
+        pushd /opt/JLink
+        wget https://www.segger.com/downloads/jlink/JLink_Linux_x86_64.deb
+        sudo dpkg -i ./JLink_Linux_x86_64.deb
+        popd
+    fi
+
+    # Scout suite
+    # YOLO
+    pipx install scoutsuite
+
+    # NRF Connect
+    if [ ! -f /opt/nrfconnect/nrfconnect-5.1.0-x86_64.appimage ]; then 
+        sudo mkdir -p /opt/nrfconnect
+        sudo chown -R $USER:$USER /opt/nrfconnect
+        pushd /opt/nrfconnect
+        wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-connect-for-desktop/5-1-0/nrfconnect-5.1.0-x86_64.appimage
+        popd
+    fi
+
+
+    # Customisation
+    if [ "$SHELL" != "/bin/zsh" ]; then
+        echo "Changing the current user's shell to zsh..."
+        sudo chsh -s /usr/bin/zsh $USER
+        echo "Shell changed to zsh. Please log out and log back in for the change to take effect."
+    fi
+    add_rc_path "$HOME/go/bin"
+    add_rc_path "$HOME/.local/bin"
+    add_line_if_not_exists "eval \$(thefuck --alias)" "$HOME/.zshrc"
+    wget https://github.com/Blackfell/ansible-hax/raw/refs/heads/main/roles/bf_arch_base/files/vimrc -o ~/.vimrc
+    wget https://github.com/Blackfell/ansible-hax/blob/main/roles/bf_arch_desktop/files/BFBackground.png?raw=true -o ~/BFBackground.png
+    gsettings set org.gnome.desktop.background picture-uri "file://$HOME/BFBAckground.png"
+}
+
+install_go_tools(){
+    # GO Tools
+    go install -v github.com/tomnomnom/httprobe@latest
+    go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+    go install -v github.com/sensepost/gowitness@latest
+    go install -v github.com/tomnomnom/assetfinder@latest
+    go install -v github.com/tomnomnom/meg@latest
+    go install -v github.com/tomnomnom/gron@latest
+    go install -v github.com/tomnomnom/waybackurls@latest
+    go install -v github.com/harleo/knockknock@latest
+    go install -v github.com/owasp-amass/amass/v3/...@latest
+    # And SSH brute forcing
+    go install ktbs.dev/ssb@latest
+}
+
+install_git_tools(){
+#### GIT TOOLS ####
+    # Helper scripts
+    clone_or_update_repo https://github.com/lapolis/helper-scripts
+    add_rc_path "/opt/helper-scripts"
+    # Ultimate Nmap Parser
+    clone_or_update_repo https://github.com/shifty0g/ultimate-nmap-parser
+    add_rc_path "/opt/ultimate-nmap-parser"
+    if [ ! -d "/opt/ultimate-nmap-parser/venv" ]; then
+        echo "[+] - Installing Nmap Parser Deps..."
+        pushd "/opt/ultimate-nmap-parser/"
+        python3 -m venv venv
+        source venv/bin/activate
+        python3 -m pip install -r requirements.txt
+        popd
+    else
+        echo "[+] - Skipping Nmap Parser Deps. Already done..."
+    fi
+    # Binwalk v3.1.0
+    clone_or_update_repo "https://github.com/ReFirmLabs/binwalk" "v3.1.0"
+    if [ ! -f "/opt/binwalk_v3.1.0/binwalkv3" ]; then
+        pushd "/opt/binwalk_v3.1.0"
+        python3 -m venv venv
+        source venv/bin/activate
+        sudo dependencies/ubuntu.sh
+        rustup deafult stable
+        cargo build --release
+        deactivate
+        popd
+        ln -s "/opt/binwalk_v3.1.0/target/release/binwalk" "/opt/binwalk_v3.1.0/binwalkv3"
+    else
+        echo "[+] - Skipping Binlwalk build. Already done..."
+    fi
+    add_rc_path "/opt/binwalk_v3.1.0/"
+
+    #bloodhound - old version  ly4k with certipy support
+    sudo mkdir -p /opt/bloodhoundly4k && sudo chown -R ${USER}:${USER} /opt/bloodhoundly4k
+    if [ ! -f /opt/bloodhoundly4k/Bloodhound ]; then
+        wget https://github.com/ly4k/BloodHound/releases/download/v4.2.0-ly4k/BloodHound-linux-x64.zip -O /opt/bloodhoundly4k/Bloodound-x64lin.zip
+        7z x /opt/bloodhoundly4k/Bloodound-x64lin.zip -o/opt/bloodhoundly4k
+        # Sadly there's a directory in the zip
+        mv /opt/bloodhoundly4k/BloodHound-linux-x64/* /opt/bloodhoundly4k/
+        add_rc_path /opt/bloodhoundly4k/
+    fi
+
+    # Not-Really-Git-Ghidra (used to do Nessus but now separate)
+    sudo mkdir -p /opt/Ghidra11.2.1 && sudo chown -R $USER:$USER /opt/Ghidra11.2.1
+    pushd /opt/Ghidra11.2.1
+    wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.2.1_build/ghidra_11.2.1_PUBLIC_20241105.zip -o ghidra_11.2.1_PUBLIC_20241105.zip
+    7z x ghidra_11.2.1_PUBLIC_20241105.zip
+    popd
+
+    # Not-Really-git-Jadx
+    sudo mkdir -p /opt/jadx && sudo chorn $USER:$USER /opt/jadx
+    pushd /opt/jadx
+    wget https://github.com/skylot/jadx/releases/download/v1.5.1/jadx-1.5.1.zip -o jadx-1.5.1.zip
+    7z x jadx-1.5.1.zip
+    popd
+
+    # Not-Really-Git-proxmark3
+    sudo apt install -y --no-install-recommends git ca-certificates build-essential pkg-config \
+        libreadline-dev gcc-arm-none-eabi libnewlib-dev qtbase5-dev \
+        libbz2-dev liblz4-dev libbluetooth-dev libpython3-dev libssl-dev libgd-dev
+    sudo systemctl stop ModemManager
+    sudo systemctl disable ModemManager
+    sudo apt remove modemmanager
+    clone_or_update_repo https://github.com/RfidResearchGroup/proxmark3
+    pushd /opt/proxmark3
+    make accessrights # Give write to serial device
+    make clean && make -j
+    popd 
+
+    #john
+    clone_or_update_repo https://github.com/openwall/john
+    pushd /opt/john
+    /configure && make -sj$(nproc)
+    popd
+
+    #linux-router - TODO add custom redirect scripts
+    clone_or_update_repo https://github.com/garywill/linux-router
+
+    # Sniffrom
+    clone_or_update_repo https://github.com/alainiamburg/sniffROM
+
+    # Ysoserial
+    clone_or_update_repo https://github.com/frohoff/ysoserial
+
+    # Snafflepy
+    clone_or_update_repo https://github.com/asmtlab/snafflepy
+    add_rc_path /opt/snafflepy
+
+    # ESP32 image parser 
+    clone_or_update_repo https://github.com/tenable/esp32_image_parser
+    for pkg in $(cat /opt/esp32_image_parser); do pipx install $pkg; done
+    add_rc_path /opt/esp32_image_parser
+
+    # Radamsa
+    sudo apt-get install gcc make git wget -y
+    clone_or_update_repo https://gitlab.com/akihe/radamsa
+    if [ ! -f /opt/radamsa/bin/radamsa ]; then
+        pushd /opt/radamsa
+        sudo make install 
+        popd
+    fi
+    add_rc_path /opt/radamsa/bin/radamsa
+
+    # PEASS-ng
+    clone_or_update_repo https://github.com/peass-ng/PEASS-ng
+    if [ ! -f /opt/PEASS-ng/linpeas.sh ]; then
+        pushd /opt/PEASS-ng
+        wget https://github.com/peass-ng/PEASS-ng/releases/download/20241205-c8c0c3e5/linpeas.sh
+        wget https://github.com/peass-ng/PEASS-ng/releases/download/20241205-c8c0c3e5/linpeas_fat.sh
+        wegt https://github.com/peass-ng/PEASS-ng/releases/download/20241205-c8c0c3e5/linpeas_linux_amd64
+        wget https://github.com/peass-ng/PEASS-ng/releases/download/20241205-c8c0c3e5/winPEASany.exe
+        wget https://github.com/peass-ng/PEASS-ng/releases/download/20241205-c8c0c3e5/winPEAS.bat
+        wget https://github.com/peass-ng/PEASS-ng/releases/download/20241205-c8c0c3e5/winPEASany_ofs.exe
+        popd
+    fi
+
+    # Flashprog
+    clone_or_update_repo https://github.com/SourceArcade/flashprog
+    if [ ! -f /opt/flashprog/flashprog ]; then 
+        pushd /opt/flashprog 
+        sudo apt install libpci-dev libftdi-dev libusb-1.0-0 libusb-dev libjaylink-dev  libgpiod-dev pkgconf -y
+        make 
+        popd
+    fi
+
+
+}
+
+####### MAIN ########
+
+# Determine the OS
+if grep -iq "ubuntu" /etc/os-release; then
+    OS="ubuntu"
+elif grep -iq "kali" /etc/os-release; then
+    OS="kali"
+else
+    echo "[!] This script is for Ubuntu or Kali. If you want to use it with another OS, edit it yourself."
+    exit 1
+fi
+
+generic_setup
+install_git_tools
+install_go_tools
+
+# OS specific steps to finish up
+if [ $OS = "ubuntu" ]; then
+    echo "Detected Ubuntu. Installing would-be Kali shit."
+    # Seclists
+    clone_or_update_repo git clone https://github.com/danielmiessler/SecLists.git
+    sudo ln -s /opt/SecLists /usr/share/seclists/
+    # DNSChef
+    clone_or_update_repo https://github.com/iphelix/dnschef  
+    add_rc_path /opt/dnschef    
+    # Responder
+    clone_or_update_repo https://github.com/SpiderLabs/Responder
+    add_rc_path /opt/Responder
+    # Nessus
+    curl --request GET --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.8.3-ubuntu1604_amd64.deb'  --output "$HOME/Nessus-10.8.3-ubuntu1604_amd64.deb"
+    sudo dpkg -i "$HOME/Nessus-10.8.3-ubuntu1604_amd64.deb"
+    # Impacket
+    pipx install impacket
+
+elif [ $OS = "kali" ]; then
+    echo "Detected Kali Linux. Installing Kali specifics."
+    # Ensure this is set in $HOME/.config/qterminal.org/qterminal.ini ApplicationTransparency=0
+    sed -i '/^ApplicationTransparency=/c\ApplicationTransparency=0' "$HOME/.config/qterminal.org/qterminal.ini" || echo "ApplicationTransparency=0" >> "$HOME/.config/file.ini"
+    # Nessus
+    curl --request GET --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.8.3-debian10_amd64.deb' --output "$HOME/Nessus-10.8.3-debian10_amd64.deb"
+    sudo dpkg -i "$HOME/Nessus-10.8.3-ubuntu1604_amd64.deb"
+    
+    #rtl8812au - Kali and wifitre helpers
+    sudo apt install -y linux-headers-amd64 realtek-rtl88xxau-dkms hcxdumptool hcxtools
+
+fi
