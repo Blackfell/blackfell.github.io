4802026949ef2480696be644028ad6da2f47ff58
blackfell <43939253+Blackfell@users.noreply.github.com>
2025-07-22 13:35:12 +0100

    Update strap.sh
    nessus link fix

--- strap.sh
+++ strap.sh
@@ -111,7 +111,7 @@ generic_setup() {
         add_rc_path /opt/Responder
         # Nessus
 	if [ ! -f /opt/nessus/sbin/nessusd ] ; then
-		curl --request GET --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.8.3-ubuntu1604_amd64.deb'  --output "$HOME/Nessus-10.8.3-ubuntu1604_amd64.deb"
+		curl --request GET --url 'https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/26040/download?i_agree_to_tenable_license_agreement=true'  --output "$HOME/Nessus-10.8.3-ubuntu1604_amd64.deb"
 	        sudo dpkg -i "$HOME/Nessus-10.8.3-ubuntu1604_amd64.deb"
 	else
  		echo "[+] Nessus already here, skipping install..."
